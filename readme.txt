4ca40f22d375cf32cafb1622ef73c9081f0dda47
Sourish Banerjee <sourish18banerjee@gmail.com>
2017-08-19 23:50:11 +0100

    First commit

---
+++ readme.txt
@@ -0,0 +1,36 @@
+Apparel Classification with Style
+==================================
+This is the apparel data set used in the ACCV 2012 paper.
+
+@inproceedings{bossard12,
+  author = {Bossard, Lukas and Dantone, Matthias and Leistner, Christian and Wengert, Christian and Quack, Till and {Van Gool}, Luc},
+  booktitle = {11th Asian Conference on Computer Vision (ACCV)},
+  title = {{Apparel Classification with Style}},
+  url = {http://www.vision.ee.ethz.ch/publications/papers/proceedings/eth\_biwi\_00974.pdf},
+  year = {2012}
+}
+
+
+This database is built from a sub set of cropped versions of ImageNet and thus
+their licensing applies which is included below:
+
+You (the "Researcher"), have requested a copy of or access to the ImageNet 
+database (the "Database"), which includes copy of the images in their original 
+resolutions and data derived from the images, at Princeton University and, 
+in exchange for such copy or access, agree to the following terms and 
+conditions:
+
+    1. Researcher shall use the Database only for non-commercial research and/or 
+       educational purposes.
+    2. Princeton University makes no representations or warranties regarding the 
+       Database, including but not limited to warranties of non-infringement, 
+       merchantability or fitness for a particular purpose.
+    3. Researcher shall defend and indemnify Princeton University, including its 
+       employees, Trustees and officers, and agents, against any claims arising 
+       from Researcher's use of the Database.
+    4. Researcher may provide research associates and colleagues with access to 
+       the Database provided that they have also agreed to be bound by these 
+       Terms of Access.
+    5. Princeton University reserves the right to terminate access to the 
+       Database at any time.
+
---
+++ transferlearningVGG16.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 23:06:10 2017
+
+@author: sourish
+"""
+
+#from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image # for image preprocessing
+import pandas as pd # for data manupulation
+
+
+#Reading the filelist of training images
+train_list=pd.read_table('train.txt',header=None, names= ['Filename'])
+
+#Reading the filelist of testing images
+test_list=pd.read_table('test.txt',header=None, names= ['Filename'])
+
+parent_dir='images'
+path_separator='/' # for unix like systems
+file_format='.jpg'
+train_images = [] #declare training image array
+
+for i in range (len(train_list)) :
+    
+    temp_image = image.load_img(parent_dir+path_separator+train_list['Filename'][i]+file_format,target_size=(224,224))
+    
+    temp_image = image.img_to_array(temp_image)
+    
+    train_images.append(temp_image)
+    
+import numpy as np #for data manupulation
+    
+train_images=np.array(train_images) 
+
+
+from keras.applications.vgg16 import preprocess_input
+
+train_images=preprocess_input(train_images)
+
+
+
+test_images = [] #declare test image array
+
+for i in range (len(test_list)) :
+    
+    temp_image = image.load_img(parent_dir+path_separator+test_list['Filename'][i]+file_format,target_size=(224,224))
+    
+    temp_image = image.img_to_array(temp_image)
+    
+    test_images.append(temp_image)
+    
+#import numpy as np #for data manupulation
+    
+test_images=np.array(test_images) 
+
+test_images=preprocess_input(test_images)
+
+
+#from keras.applications.vgg16 import preprocess_input
+
+
+from keras.models import Model
+
+from keras.applications.vgg16 import VGG16
+
+from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, AveragePooling2D, ZeroPadding2D, Dropout, Flatten, merge, Reshape, Activation
+
+
+
+def vgg16_model(img_rows, img_cols, channel=1, num_classes=None):
+
+    model = VGG16(weights='imagenet', include_top=True)
+
+    model.layers.pop() #get rid of prediction layer
+    model.layers.pop() #get rid of fc2 layer
+    model.layers.pop() #get rid of fc1 layer
+    
+
+    model.outputs = [model.layers[-1].output]
+
+    model.layers[-1].outbound_nodes = []
+
+    #x=Dense(num_classes, activation='softmax')(model.output)
+
+    model1=Model(model.input,model.outputs)
+    
+    x=Dense(num_classes, activation='softmax')(model1.output)
+    
+    model1=Model(model1.input,x)
+
+#To set the first 8 layers to non-trainable (weights will not be updated)
+
+    for layer in model1.layers[:17]:
+
+        layer.trainable = False
+
+# Learning rate is changed to 0.001
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model1.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model1
+
+#Processing the labels
+train_labels = np.asarray(pd.read_table('train.txt',delimiter='/',header=None,names=['Label','Sub_Dir','Filename'])['Label'])
+
+test_labels = np.asarray(pd.read_table('test.txt',delimiter='/',header=None,names=['Label','Sub_Dir','Filename'])['Label'])
+
+from sklearn.preprocessing import LabelEncoder
+from keras.utils.np_utils import to_categorical
+
+le = LabelEncoder()
+
+train_labels = le.fit_transform(train_labels)
+
+train_labels = to_categorical(train_labels)
+
+train_labels = np.array(train_labels)
+
+test_labels = le.fit_transform(test_labels)
+
+test_labels = to_categorical(test_labels)
+
+test_labels = np.array(test_labels)
+
+
+img_rows, img_cols = 224, 224 # Resolution of inputs
+channel = 3
+num_classes = 15
+batch_size = 256
+nb_epoch = 1
+
+model = vgg16_model(img_rows, img_cols, channel, num_classes)
+
+model.summary()
+
+
+model.fit(train_images, train_labels,batch_size=batch_size,epochs=nb_epoch,shuffle=True,verbose=1,validation_data=(test_images, test_labels))
